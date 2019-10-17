2e27a6d8c65de6fc19206e3b6108db8be89f9408
김희진 <insiderhj@gmail.com>
2019-10-17 16:18:02 +0900

    add projects

---
+++ project3/src/main.c
@@ -0,0 +1,42 @@
+#include "bpt.h"
+
+int fds[10];
+
+int main( int argc, char ** argv ) {
+    char instruction;
+    int64_t key;
+    char value[VALUE_SIZE];
+    int file_num = 0;
+    open_table("database_insiderhj");
+
+    printf("> ");
+    while (scanf("%c", &instruction) != EOF) {
+        switch (instruction) {
+            case 'd':
+                scanf("%ld", &key);
+                db_delete(fds[0], key);
+                break;
+            case 'i':
+                scanf("%ld %s", &key, value);
+                db_insert(fds[0], key, value);
+                break;
+	        case 'f':
+                scanf("%ld", &key);
+                if (db_find(fds[0], key, value) == NOT_FOUND) printf("key %ld not found\n", key);
+                else printf("%ld: %s\n", key, value);
+                break;
+            case 'p':
+                print_tree(fds[0]);
+                break;
+            case 'q':
+                while (getchar() != (int)'\n');
+                return 0;
+                break;
+        }
+        while (getchar() != (int)'\n');
+        printf("> ");
+    }
+    printf("\n");
+    return 0;
+}
+
